b5465ff01b1f6a82a3600def06b0c670ea838faf
jonni <gabisov.muratos@gmail.com>
2020-04-26 23:46:42 +0300

    Создал новую библиотеку классов,для работы с файлами

---
+++ AIST.BL/FileManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.IO;
+
+namespace AIST.BL
+{
+    class FileManager
+    {
+        //Открытие текста
+        public string GetText(string fileName, Encoding encoding)
+        {
+
+            string text = File.ReadAllText(fileName, encoding);
+            return text;
+        }
+        public string GetText(string fileName)
+
+        {
+
+            string text = File.ReadAllText(fileName);
+            return text;
+        }
+        //Сохранение текста
+        public void SaveText(string fileName, string text, Encoding encoding)
+        {
+            File.WriteAllText(fileName, text, encoding);
+        }
+        public void SaveText(string fileName, string text)
+        {
+            File.WriteAllText(fileName, text);
+        }
+    }
+}
